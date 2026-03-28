91c67290fe0f9bab4bb51d7746b6c7c73ee19744
h1101241011-cloud <h1101241011@student.untan.ac.id>
2026-03-28 11:27:37 +0700

    git update

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -4,7 +4,7 @@ import { createClient } from "@libsql/client";
 
 const client = createClient({
   url: "libsql://monorepo-h1101241011-cloud.aws-eu-west-1.turso.io",
-  authToken: "TOKEN_KAMU_YANG_PANJANG",
+  authToken: "eyJheyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzQ1ODYwNTUsImlkIjoiMDE5ZDJkOTEtY2YwMS03MDllLWFmMTktOTAxNmY4YjBlZmM3IiwicmlkIjoiYzI3MjNmNDAtODBlZi00YjdkLTlhZDUtOTAzYTgyNzY1ZTI4In0.2zC2aj6brdXW9sKYps8oGPxHIPP8fd1RVHfwUdLuSWEKzZlyctCBYd_OVYq7M8uTbxvsBiFc0Ht2thiwRHMJCQbGciOiJFZERTQSIsInR5cCI",
 });
 
 const adapter = new PrismaLibSql(client as any);
